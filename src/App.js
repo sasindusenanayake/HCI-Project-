46da5f4407ca3bce0606b01de31025f3cd37e498
bawanthathilan <bawantharathnayaka@gmail.com>
2020-09-29 17:31:50 +0530

    add rourt dom dependency

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from'./components/footer/footer';
 import Header from './components/Header/header'
+import Driveaway from './components/pages/driveawaygates'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+}from 'react-router-dom'
 
 function App() {
   return (
@@ -10,6 +16,14 @@ function App() {
     <Header></Header>
     <br></br>
     <br></br>
+    <br></br>
+      <Router>
+        <Switch>
+          <Route path="/drivawaygates"><Driveaway></Driveaway></Route>
+          <Route path="/cart"></Route>
+          <Route path="/gates"></Route>
+        </Switch>
+      </Router>
     <br></br>
     <Footer></Footer>  
     </div>
