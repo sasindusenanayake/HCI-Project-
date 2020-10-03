3c445fce7b907a44f9d20911c13a4e74ee4f8c85
sasindu  senanayake <senanayakesasindu@gmail.com>
2020-10-03 15:32:30 +0530

    1st hci

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from'./components/footer/footer';
 import Header from './components/Header/header'
 import Driveaway from './components/pages/driveawaygates'
 import Sasindu from './components/pages/test'
+import Hci1 from './components/pages/home';
 import {
     BrowserRouter as Router,
     Switch,
@@ -15,14 +16,19 @@ function App() {
   return (
     <div className="App">
     <Header></Header>
+    <br></br>
+    <br></br>
+    <br></br>
       <Router>
         <Switch>
           <Route path="/test"><Sasindu></Sasindu></Route>
-          <Route path="/drivawaygates"><Driveaway></Driveaway></Route>
-          <Route path="/cart"></Route>
+          <Route> path="/home"><Hci1></Hci1></Route>
+          <Route path="/drivawaygates"><Driveaway> </Driveaway></Route> 
+          <Route path="/cart"></Route> 
           <Route path="/gates"></Route>
         </Switch>
       </Router>
+    <br></br>
     <Footer></Footer>  
     </div>
   );
